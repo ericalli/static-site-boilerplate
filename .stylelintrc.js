b1f5fbd33c6db146a6328bf5b7ada7733307db82
Eric Alli <hello@ericalli.com>
2018-12-23 17:21:17 -0800

    Update paths

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   "extends": "stylelint-config-standard",
+  "plugins": ["stylelint-scss"],
+  "rules": {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true
+  },
 }
--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'cheap-eval-source-map',
   devServer: {
-    contentBase: 'src',
+    contentBase: './../src',
     watchContentBase: true,
     hot: true,
     open: true,
