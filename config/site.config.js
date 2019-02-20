bce5b8e718db68352e0adb6d779eaba297123eb0
Eric Alli <hello@ericalli.com>
2019-02-19 23:00:21 -0800

    Update default viewport tag, fixes #13

--- config/site.config.js
+++ config/site.config.js
@@ -21,7 +21,7 @@ const config = {
   googleAnalyticsUA: '',
 
   // The viewport meta tag added to your HTML page's <head> tag
-  viewport: 'width=device-width,minimum-scale=1,maximum-scale=1',
+  viewport: 'width=device-width,initial-scale=1',
 
   // Source file for favicon generation. 512x512px recommended.
   favicon: path.join(ROOT, '/src/images/favicon.png'),
